f58149e2d142b282b0f0d4968f3c35c8c653e292
paain999 <theartistdavidj@gmail.com>
2023-12-28 13:32:06 +0100

    fixed strnstr

--- src/libft.c
+++ src/libft.c
@@ -6,7 +6,7 @@
 /*   By: dajimene <dajimene@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 01:22:42 by dajimene          #+#    #+#             */
-/*   Updated: 2023/12/27 18:08:45 by dajimene         ###   ########.fr       */
+/*   Updated: 2023/12/28 13:27:47 by dajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strnstr(const char *s1, const char *s2)
 
 	i = 0;
 	if (*s2 == '\0' || s1 == s2)
-		return ((char *)s1);
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		j = 0;
